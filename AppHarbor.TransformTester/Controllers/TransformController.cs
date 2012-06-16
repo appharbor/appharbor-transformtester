4669daa601cd84d31c8b5517f5793a725893f8ce
Rick Yoesting <rick.yoesting@gmail.com>
2012-06-16 09:07:13 -0500

    rename exception variables; use tabs instead of spaces

--- AppHarbor.TransformTester/Controllers/TransformController.cs
+++ AppHarbor.TransformTester/Controllers/TransformController.cs
@@ -12,47 +12,46 @@ namespace AppHarbor.TransformTester.Controllers
 		[ValidateInput(false)]
 		public ActionResult Create(string webConfigXml, string transformXml)
 		{
-            try
-            {
-                var transformation = new XmlTransformation(transformXml, false, null);
-                var document = new XmlDocument();
-                document.LoadXml(webConfigXml);
-                var success = transformation.Apply(document);
-                if (success)
-                {
-                    var stringBuilder = new StringBuilder();
-                    var xmlWriterSettings = new XmlWriterSettings();
-                    xmlWriterSettings.Indent = true;
-                    xmlWriterSettings.IndentChars = "    ";
-                    using (var xmlTextWriter =
-                        XmlTextWriter.Create(stringBuilder, xmlWriterSettings))
-                    {
-                        document.WriteTo(xmlTextWriter);
-                    }
-                    return Content(stringBuilder.ToString(), "text/xml");
-                }
-                else
-                {
-                    return ErrorXml("Transformation failed for unkown reason");
-                }
-            }
-            catch (XmlTransformationException ex)
-            {
-                return ErrorXml(ex.Message);
-            }
-            catch (XmlException exception)
-            {
-                return ErrorXml(exception.Message);
-            }
+			try
+			{
+				var transformation = new XmlTransformation(transformXml, false, null);
+				var document = new XmlDocument();
+				document.LoadXml(webConfigXml);
+				var success = transformation.Apply(document);
+				if (success)
+				{
+					var stringBuilder = new StringBuilder();
+					var xmlWriterSettings = new XmlWriterSettings();
+					xmlWriterSettings.Indent = true;
+					xmlWriterSettings.IndentChars = "	";
+					using (var xmlTextWriter =
+						XmlTextWriter.Create(stringBuilder, xmlWriterSettings))
+					{
+						document.WriteTo(xmlTextWriter);
+					}
+					return Content(stringBuilder.ToString(), "text/xml");
+				}
+				else
+				{
+					return ErrorXml("Transformation failed for unkown reason");
+				}
+			}
+			catch (XmlTransformationException xmlTransformationException)
+			{
+				return ErrorXml(xmlTransformationException.Message);
+			}
+			catch (XmlException xmlException)
+			{
+				return ErrorXml(xmlException.Message);
+			}
 		}
 
-        private ContentResult ErrorXml(string errorMessage)
-        {
-            return Content(
-                new XDocument(
-                    new XElement("error", errorMessage)
-                ).ToString(), "text/xml");
-
-        }
+		private ContentResult ErrorXml(string errorMessage)
+		{
+			return Content(
+					new XDocument(
+						new XElement("error", errorMessage)
+				).ToString(), "text/xml");
+		}
 	}
 }
