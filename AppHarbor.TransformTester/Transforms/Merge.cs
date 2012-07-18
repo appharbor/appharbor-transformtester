2a91f9c749beb21f52df12dcb323831be4397f87
Michael Friis <friism@gmail.com>
2012-07-17 17:22:24 -0700

    add custom merge

---
+++ AppHarbor.TransformTester/Transforms/Merge.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using System.Xml;
+using Microsoft.Web.Publishing.Tasks;
+
+namespace AppHarbor.TransformTester.Transforms
+{
+	public class Merge : Transform
+	{
+		public Merge()
+			: base(TransformFlags.UseParentAsTargetNode)
+		{
+		}
+
+		protected override void Apply()
+		{
+			Apply((XmlElement)TargetNode, (XmlElement)TransformNode);
+		}
+
+		public static void Apply(XmlElement targetElement, XmlElement transformElement)
+		{
+			var targetChildElement = targetElement.ChildNodes.OfType<XmlElement>().FirstOrDefault(x => x.LocalName == transformElement.LocalName);
+			if (targetChildElement == null)
+			{
+				targetElement.AppendChild(transformElement);
+				return;
+			}
+
+			foreach (var transformChildElement in transformElement.ChildNodes.OfType<XmlElement>())
+			{
+				Apply(targetChildElement, transformChildElement);
+			}
+		}
+
+	}
+}
