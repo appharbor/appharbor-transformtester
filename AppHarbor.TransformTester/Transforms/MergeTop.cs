e657abd84c0eb70d7d1aac61ce8c5c297a8b39e1
Michael Friis <friism@gmail.com>
2012-07-18 13:11:16 -0700

    add mergetop

---
+++ AppHarbor.TransformTester/Transforms/MergeTop.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+using System.Xml;
+
+namespace AppHarbor.TransformTester.Transforms
+{
+	public class MergeTop : Merge
+	{
+		protected override void InsertTransformElement(XmlElement targetElement, XmlElement transformElement)
+		{
+			targetElement.InsertBefore(transformElement, targetElement.ChildNodes.OfType<XmlNode>().FirstOrDefault());
+		}
+	}
+}
